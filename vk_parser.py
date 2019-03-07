e77bea8988c7005759346ed27470085dc979df57
Evgenii Koleda <j.koleda2012@gmail.com>
2019-03-07 22:12:54 +0300

    Added func export_to_csv for saving parsed data into csv-file.

--- vk_parser.py
+++ vk_parser.py
@@ -10,7 +10,36 @@
 # ------------------------------------------------------
 
 import requests
+import csv
+from datetime import datetime
 
+
+def export_to_csv(all_posts):
+    file_name = input('Please enter name of file .csv\n')
+    method = input('Choose:\n1 - Rewrite file\n2 - Append file\nOther to exit\n')
+    if method == '1':
+        mode = 'w'
+
+    elif method == '2':
+        mode = 'a'
+
+    else:
+        return
+
+    with open(file_name, mode, encoding='utf-8') as csv_file:
+        a_pen = csv.writer(csv_file)
+        if mode == 'w':
+            a_pen.writerow(('Date', 'Text', 'Likes', 'Comments', 'Views', 'Image URL'))
+        for post in all_posts:
+            try:
+                if post['attachments'][0]['type']:
+                    img_url = post['attachments'][0]['photo']['sizes'][-1]['url']
+                else:
+                    img_url = 'pass'
+            except:
+                img_url = 'pass'
+            a_pen.writerow((datetime.utcfromtimestamp(post['date']).strftime('%Y-%m-%d %H:%M:%S'), post['text'],
+                           post['likes']['count'], post['comments']['count'], post['views']['count'], img_url))
 
 def take_posts():
     token = '61a1844761a1844761a18447e161c89f63661a161a184473ddee16e0614c997c0a312fe'
@@ -42,3 +71,4 @@ def take_posts():
 
 
 all_posts = take_posts()
+export_to_csv(all_posts)
